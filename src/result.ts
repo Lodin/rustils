8ca40c34c54e78b97d06c13ef5417a80b1bdfe6e
Vlad Rindevich <rindevich.vs@gmail.com>
2017-01-27 13:24:00 +0300

    feat(result): finish

--- src/result.ts
+++ src/result.ts
@@ -55,4 +55,58 @@ export class Result<T, E> implements Match<ResultMatcher<T, E>> {
       ? res
       : <Result<null, E>><any>this; // Err(this.error)
   }
+
+  public andThen<U>(cb: (value: T) => Result<U|null, E|null>): Result<U|null, E|null> {
+    return this.isOk
+      ? cb(<T>this.value)
+      : <Result<null, E>><any>this;
+  }
+
+  public or<F>(res: Result<T|null, F|null>): Result<T|null, F|null> {
+    return this.isOk
+      ? <Result<T|null, F|null>><any>this
+      : res;
+  }
+
+  public orElse<F>(cb: (error: E) => Result<T|null, F|null>): Result<T|null, F|null> {
+    return this.isOk
+      ? <Result<T|null, F|null>><any>this
+      : cb(<E>this.error);
+  }
+
+  public unwrapOr(optb: T): T {
+    return this.isOk
+      ? <T>this.value
+      : optb;
+  }
+
+  public unwrapOrElse(cb: (error: E) => T) {
+    return this.isOk
+      ? <T>this.value
+      : cb(<E>this.error);
+  }
+
+  public unwrap(): T {
+    if (this.isOk) {
+      return <T>this.value;
+    }
+
+    throw this.error;
+  }
+
+  public unwrapErr(): E {
+    if (this.isErr) {
+      return <E>this.error;
+    }
+
+    throw this.value;
+  }
+
+  public expect(msg: string): T {
+    if (this.isOk) {
+      return <T>this.value;
+    }
+
+    throw new Error(`${msg}: ${(<E>this.error).toString()}`)
+  }
 }
