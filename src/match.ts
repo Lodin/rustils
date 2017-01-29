9ce2c53bdf5bee3dfe03cdfb69f3112a6a60f22e
Vlad Rindevich <rindevich.vs@gmail.com>
2017-01-29 18:42:23 +0300

    docs(match): add documentation for match

--- src/match.ts
+++ src/match.ts
@@ -1,3 +1,7 @@
 export interface Match {
+  /**
+   * Matches instance to the received matcher and executes appropriate branch.
+   * @param matcher an object to perform pattern matching
+   */
   match<T extends {}, U>(matcher: T): U;
 }
